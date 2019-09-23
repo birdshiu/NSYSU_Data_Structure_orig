1d17a92b15b45081a137c656e81cab2a59801f73
25077667 <zxc25077667@gmail.com>
2019-09-24 01:35:48 +0800

    remove useless line and sort

--- exercise/CPE/10420/a.cpp
+++ exercise/CPE/10420/a.cpp
@@ -9,23 +9,19 @@ int main() {
     vector<pair<string, int>> countryMap;
     while (numLine--) {
         string country, trash;
-
-        // only read the first word, ignore all after the first word
         cin >> country >> trash >> trash;
         
         auto findIndex = countryMap.begin();
-        for (; findIndex != countryMap.end(); findIndex++) {
+        for (; findIndex != countryMap.end(); findIndex++)
             if (findIndex->first == country)
                 break;
-        }
 
         if (findIndex != countryMap.end())
             findIndex->second++;
         else
             countryMap.push_back(make_pair(country, 1));
-        //
     }
-    
+    sort(countryMap.begin(), countryMap.end());
     for (auto i : countryMap)
         cout << i.first << " " << i.second << endl;
     return 0;
