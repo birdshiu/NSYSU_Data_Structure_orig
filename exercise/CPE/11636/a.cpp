3ab0ec5714b23ebbe6bd3cd433915e32a189a2cf
25077667 <zxc25077667@gmail.com>
2019-09-24 16:51:25 +0800

    clean code

--- exercise/CPE/11636/a.cpp
+++ exercise/CPE/11636/a.cpp
@@ -1,4 +1,3 @@
-#include <cmath>
 #include <iostream>
 using namespace std;
 
@@ -6,7 +5,6 @@ int main() {
     int num, cases = 1;
     while (cin >> num && num > 0) {
         int answer = 0;
-        //cout << num << endl;
         for (; num; num = num >> 1)
             answer++;
         printf("Case %d: %d\n", cases++, answer);
