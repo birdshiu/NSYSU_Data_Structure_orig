3bd215b9ef4d4243a48452ece3eb8db24ed5a28a
25077667 <zxc25077667@gmail.com>
2019-12-14 02:28:53 +0800

    make zenity more fun

--- 108Course/08/zenity/shellGUI.sh
+++ 108Course/08/zenity/shellGUI.sh
@@ -4,7 +4,7 @@ FILE="$(zenity --file-selection --title='Select your File')"
 zenity --question --text="operation?" --ok-label="compress" --cancel-label="decompress"
 ANS=$?
 if [ "$ANS" -eq 0 ]; then
-    ../huffman.elf ${FILE} -c
+    ../huffman.elf ${FILE} -c | zenity --progress --pulsate --time-remaining
 else
-    ../huffman.elf ${FILE} -d
+    ../huffman.elf ${FILE} -d | zenity --progress --pulsate --time-remaining
 fi
